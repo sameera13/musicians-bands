5890344e659d6c9ac8768523982b1293a1a6505d
Joshua Benavides <joshua.benavides@verizon.com>
2025-05-14 13:00:18 -0500

    Did the song portion of the workshop

--- index.test.js
+++ index.test.js
@@ -41,4 +41,31 @@ describe('Band, Musician, and Song Models', () => {
         // TODO - test deleting a musician
         expect('NO TEST').toBe('EXPECTED VALUE HERE');
     })
-})
+
+    test('can create a Song', async () => {
+        // TODO - test creating a band
+        const testSong = await Song.create(
+            {title:"Cool Song", year: 1999, length: 20});
+        expect(testSong.title).toBe("Cool Song");
+        expect(testSong.year).toBe(1999);
+        expect(testSong.length).toBe(20);
+    })
+    test('can update a Song', async () => {
+        // TODO - test updating a song
+        const testSong = await Song.create(
+            {title:"Cool Song", year: 1999, length: 20});
+        const updateSong = await testSong.update({
+        title: "Updated cooler song",
+        length: 30 });
+        expect(updateSong.title).toBe("Updated cooler song");
+        expect(updateSong.length).toBe(30);
+    })
+    test('can delete a Song', async () => {
+        // TODO - test deleting a song
+        const testSong = await Song.create(
+            {title:"Cool Song", year: 1999, length: 20});
+            await testSong.destroy();
+            const deletedSong = await Song.findByPk(testSong.id);
+          expect(deletedSong).toBeNull();
+    });
+});
