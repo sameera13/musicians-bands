d007aecd2d3196214c95ae42f1c44b70eea5c26c
jordanharris0 <jordanharris1833@yahoo.com>
2025-05-14 12:19:42 -0500

    finished Band CRUD and tests

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const { Band } = require('./models/Band')
-const { Musician } = require('./models/Musician')
-const { Song } = require("./models/Song")
+const { Band } = require("./models/Band");
+const { Musician } = require("./models/Musician");
+const { Song } = require("./models/Song");
+const { sequelize } = require("./db");
 // Define associations here
 
+async function initialize() {
+  await sequelize.sync({ force: true });
+  //create a band
+  const testBand = await Band.create({ name: "Test Band", genre: "Rock" });
+  console.log(testBand);
 
+  //update the band
+  let updatedBand = await testBand.update({
+    name: "Updated Band",
+    genre: "Pop",
+  });
+  console.log(updatedBand);
+
+  //delete the band
+  await testBand.destroy();
+  const deletedBand = await Band.findByPk(testBand.id);
+  console.log(deletedBand);
+}
+initialize();
 
 module.exports = {
-    Band,
-    Musician,
-    Song
+  Band,
+  Musician,
+  Song,
 };
